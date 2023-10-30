bee0bfc08c45a5d77389709a805c56b902888a51
tiagoabranges <tiagoabranges@gmail.com>
2023-10-30 18:41:26 -0300

    feat: created home screen

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import HeroList from '../../Components/HeroList';
+import './home.css';
+function Home() {
+  return (
+    <div className="home">
+      <h1>pagina inicial</h1>
+      <HeroList />
+    </div>
+  );
+}
+
+export default Home;
